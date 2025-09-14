69d82b092223a5d8f0f6ec4ab080390a464dae19
Harish Mallikarjuna <harish.mallikarjuna@ascendion.com>
2025-09-14 14:01:58 +0530

    Adding s3 terraform config

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "my_bucket" {
     }
 }
 
-resource "aws_s3_public_access_block" "block_public_access" {
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
     bucket = aws_s3_bucket.my_bucket.id
     block_public_acls = true
     block_public_policy = true
