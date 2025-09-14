4719238e20efc7b44cecd72fd5b42249294b5b33
Harish Mallikarjuna <harish.mallikarjuna@ascendion.com>
2025-09-14 17:15:46 +0530

    adding provider for random password

--- modules/rds/providers.tf
+++ modules/rds/providers.tf
@@ -1,12 +1,20 @@
 terraform {
     required_providers {
         aws = {
-            source = "hashicorrp/aws"
+            source = "hashicorp/aws"
             version = ">= 5.58.0"
+        }
+        random = {
+            source = "hashicorp/random"
+            version = ">= 3.4.3"
         }
     }
 }
 
 provider "aws" {
     region = "ap-south-1"
+}
+
+provider "random" {
+    
 }
--- rds/providers.tf
+++ rds/providers.tf
@@ -4,9 +4,17 @@ terraform {
             source = "hashicorp/aws"
             version = "~> 6.0"
         }
+        random = {
+            source = "hashicorp/random"
+            version = "~> 3.0"
+        }
     }
 }
 
 provider "aws" {
     region = "ap-south-1"
+}
+
+provider "random" {
+    
 }
