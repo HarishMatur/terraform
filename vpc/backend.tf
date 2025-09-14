82d5208a10d056287446491d0f4b4534af290b2e
Harish Mallikarjuna <harish.mallikarjuna@ascendion.com>
2025-09-14 13:53:44 +0530

    Adding s3 terraform config

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend s3 {
         bucket = "remote-statefile-backup"
         key = "vpc/vpc.tfstate"
-        region = var.region
+        region = "ap-south-1"
         encrypt = true
         dynamodb_table = "terraform-locks"
     }
